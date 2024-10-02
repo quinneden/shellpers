d31b321967d6e1ecd6049eb2d59e59b0acab6fd8
Quinn Edenfield <quinnyxboy@gmail.com>
2024-10-01 20:11:46 -0700

    Modified: scripts/commit.nix

--- scripts/commit.nix
+++ scripts/commit.nix
@@ -1,10 +1,9 @@
 {pkgs, ...}: pkgs.writeShellScriptBin "commit" ''
   confirm() {
     while true; do
-      read -sr -n 1 REPLY
+      read -sr -n 1 -p 'Commit with this message? [y\n]' REPLY
       case $REPLY in
         [yY]) echo ; return 0 ;;
-        [$'\x0A']) echo ; return 0 ;;
         [nN]) echo ; return 1 ;;
         *) printf " \033[31m %s \n\033[0m" "invalid input"
       esac
@@ -12,26 +11,26 @@
   }
 
   REPONAME=$(basename $PWD | tr -d '.')
-  TMPFILE=$(mktemp /tmp/git-commit-msg-$REPONAME.XXXXX)
+  TMPFILE=$(mktemp /tmp/commit-msg-$REPONAME.XXXXXXX)
 
   git status --porcelain | grep '^[MARCDT]' | sort \
-  | sed -re 's/^([[:upper:]])[[:upper:]]?[[:space:]]+/\\1:\\n/' \
+  | sed -re 's/^([[:upper:]])[[:upper:]]?[[:space:]]+/\1:\n/' \
   | awk '!x[$0]++' \
-  | sed -re 's/^([[:upper:]]:)$/\\n\\1/' \
+  | sed -re 's/^([[:upper:]]:)$/\n\1/' \
   | sed -re 's/^M:$/Modified: /' \
   | sed -re 's/^A:$/Added: /' \
   | sed -re 's/^R:$/Renamed: /' \
   | sed -re 's/^C:$/Copied: /' \
   | sed -re 's/^D:$/Deleted: /' \
   | sed -re 's/^T:$/File Type Changed: /' \
-  | tr '\n' ' ' | xargs
+  | tr '\n' ' ' | xargs \
   > $TMPFILE
 
   cat $TMPFILE
 
-  confirm 'Commit with this message? [y\n]' || exit 1
-
-  git commit -F $TMPFILE
+  if confirm; then
+    git commit -F $TMPFILE
+  fi
 
   rm -f $TMPFILE
 ''
