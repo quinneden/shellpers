eb4678eb2c2e6870a93548f7b01cc9655b96039c
Quinn Edenfield <quinnyxboy@gmail.com>
2024-10-07 05:31:17 -0700

    Modified:   .gitignore   flake.lock   nix/commit.nix   scripts/commit.nix

--- nix/commit.nix
+++ nix/commit.nix
@@ -1,47 +1,138 @@
-{pkgs, stdenv, ...}: let
-commit = pkgs.writeShellScriptBin "commit" ''
-  confirm() {
-    while true; do
-      read -sr -n 1 -p 'Commit with this message? [y\n]' REPLY
-      case $REPLY in
-        [yY]) echo ; return 0 ;;
-        [nN]) echo ; return 1 ;;
-        *) printf " \033[31m %s \n\033[0m" "invalid input"
-      esac
-    done
-  }
+{
+  pkgs,
+  stdenv,
+  ...
+}: let
+  commit = pkgs.writeShellScriptBin "commit" ''
+    git_status_json() {
+      # Declare an associative array to store files by status
+      declare -A status_files
 
-  REPONAME=$(basename $PWD | tr -d '.')
-  TMPFILE=$(mktemp /tmp/commit-msg-$REPONAME.XXXXXXX)
+      # Process each line of git status output
+      while IFS= read -r line; do
+          # Extract the status and the filename
+          status="''${line:0:2}"  # First two characters are the status
+          file="''${line:3}"      # Filename starts at the third character
 
-  git status --porcelain | grep '^[MARCDT]' | sort \
-  | sed -re 's/^([[:upper:]])[[:upper:]]?[[:space:]]+/\1:\n/' \
-  | awk '!x[$0]++' \
-  | sed -re 's/^([[:upper:]]:)$/\n\1/' \
-  | sed -re 's/^M:$/Modified: /' \
-  | sed -re 's/^A:$/Added: /' \
-  | sed -re 's/^R:$/Renamed: /' \
-  | sed -re 's/^C:$/Copied: /' \
-  | sed -re 's/^D:$/Deleted: /' \
-  | sed -re 's/^T:$/File Type Changed: /' \
-  | tr '\n' ' ' | xargs \
-  > $TMPFILE
+          # Trim spaces from the status
+          status=$(echo "$status" | xargs)
 
-  cat $TMPFILE
+          # Append the file to the corresponding status list
+          if [[ -n "''${status_files[$status]}" ]]; then
+              status_files[$status]+=", \"$file\""
+          else
+              status_files[$status]="\"$file\""
+          fi
+      done <<< "$git_status"
 
-  if confirm; then
-    git commit -F $TMPFILE
-  fi
+      # Start the JSON output with a single object
+      echo '{'
+      echo '  "git_status": {'
 
-  rm -f $TMPFILE
-'';
-in
-stdenv.mkDerivation rec {
-  name = "commit";
-  src = ./.;
-  buildInputs = [commit];
-  installPhase = ''
-    mkdir -p $out/bin
-    cp ${commit}/bin/* $out/bin
+      # Output the statuses and their file lists
+      first_entry=true
+      for status in "''${!status_files[@]}"; do
+          # Set comma for subsequent entries
+          if [ "$first_entry" = false ]; then
+              echo ','
+          fi
+          first_entry=false
+
+          # Output the status with the list of files
+          echo "    \"$status\": [''${status_files[$status]}]"
+      done
+
+      # Close the JSON object
+      echo '  }'
+      echo '}'
+    }
+
+    parse_json() {
+      modified=$(jq -r '.git_status | .M[]' < $tmp | tr '\n' ';' | sed s/';'/'\n  '/g)
+      added=$(jq -r '.git_status | .A[]' < $tmp | tr '\n' ';' | sed s/';'/'\n  '/g)
+      renamed=$(jq -r '.git_status | .R[]' < $tmp | tr '\n' ';' | sed s/';'/'\n  '/g)
+      copied=$(jq -r '.git_status | .C[]' < $tmp | tr '\n' ';' | sed s/';'/'\n  '/g)
+      deleted=$(jq -r '.git_status | .D[]' < $tmp | tr '\n' ';' | sed s/';'/'\n  '/g)
+      ftchange=$(jq -r '.git_status | .T[]' < $tmp | tr '\n' ';' | sed s/';'/'\n  '/g)
+
+      if [[ -n $added ]]; then
+        cat >> $msg <<EOF
+    Added:
+      $added
+    EOF
+      fi
+
+      if [[ -n $modified ]]; then
+        cat >> $msg <<EOF
+    Modified:
+      $modified
+    EOF
+      fi
+
+      if [[ -n $renamed ]]; then
+        cat >> $msg <<EOF
+    Renamed:
+      $renamed
+    EOF
+      fi
+
+      if [[ -n $copied ]]; then
+        cat >> $msg <<EOF
+    Copied:
+      $copied
+    EOF
+      fi
+
+      if [[ -n $deleted ]]; then
+        cat >> $msg <<EOF
+    Deleted:
+      $deleted
+    EOF
+      fi
+
+      if [[ -n $ftchange ]]; then
+        cat >> $msg <<EOF
+    Filetype changed:
+      $ftchange
+    EOF
+      fi
+    }
+
+    confirm() {
+      while true; do
+        read -sr -n 1 -p 'Commit with this message? [y\n]' REPLY
+        case $REPLY in
+          [yY]) echo ; return 0 ;;
+          [nN]) echo ; return 1 ;;
+          *) printf " \033[31m %s \n\033[0m" "invalid input"
+        esac
+      done
+    }
+
+    main() {
+      git_status=$(git status --porcelain)
+      tmp=$(mktemp -p /tmp gitstat-json.XXXXXXXXXX)
+      msg=$(mktemp -p /tmp git-commit-msg.XXXXXXXXX)
+
+      (git_status_json | jq) > $tmp
+      parse_json 2>/dev/null
+
+      cat $msg
+
+      if confirm; then
+        git commit -F $msg
+      fi
+    }
+
+    main || exit 1
   '';
-}
+in
+  stdenv.mkDerivation rec {
+    name = "cfg";
+    src = ./.;
+    buildInputs = [cfg];
+    installPhase = ''
+      mkdir -p $out/bin
+      cp ${cfg}/bin/* $out/bin
+    '';
+  }
--- scripts/commit.nix
+++ scripts/commit.nix
@@ -1,4 +1,98 @@
 {pkgs, ...}: pkgs.writeShellScriptBin "commit" ''
+  git_status_json() {
+    # Declare an associative array to store files by status
+    declare -A status_files
+
+    # Process each line of git status output
+    while IFS= read -r line; do
+        # Extract the status and the filename
+        status="''${line:0:2}"  # First two characters are the status
+        file="''${line:3}"      # Filename starts at the third character
+
+        # Trim spaces from the status
+        status=$(echo "$status" | xargs)
+
+        # Append the file to the corresponding status list
+        if [[ -n "''${status_files[$status]}" ]]; then
+            status_files[$status]+=", \"$file\""
+        else
+            status_files[$status]="\"$file\""
+        fi
+    done <<< "$git_status"
+
+    # Start the JSON output with a single object
+    echo '{'
+    echo '  "git_status": {'
+
+    # Output the statuses and their file lists
+    first_entry=true
+    for status in "''${!status_files[@]}"; do
+        # Set comma for subsequent entries
+        if [ "$first_entry" = false ]; then
+            echo ','
+        fi
+        first_entry=false
+
+        # Output the status with the list of files
+        echo "    \"$status\": [''${status_files[$status]}]"
+    done
+
+    # Close the JSON object
+    echo '  }'
+    echo '}'
+  }
+
+  parse_json() {
+    modified=$(jq -r '.git_status | .M[]' < $tmp | tr '\n' ';' | sed s/';'/'\n  '/g)
+    added=$(jq -r '.git_status | .A[]' < $tmp | tr '\n' ';' | sed s/';'/'\n  '/g)
+    renamed=$(jq -r '.git_status | .R[]' < $tmp | tr '\n' ';' | sed s/';'/'\n  '/g)
+    copied=$(jq -r '.git_status | .C[]' < $tmp | tr '\n' ';' | sed s/';'/'\n  '/g)
+    deleted=$(jq -r '.git_status | .D[]' < $tmp | tr '\n' ';' | sed s/';'/'\n  '/g)
+    ftchange=$(jq -r '.git_status | .T[]' < $tmp | tr '\n' ';' | sed s/';'/'\n  '/g)
+
+    if [[ -n $added ]]; then
+      cat >> $msg <<EOF
+  Added:
+    $added
+  EOF
+    fi
+
+    if [[ -n $modified ]]; then
+      cat >> $msg <<EOF
+  Modified:
+    $modified
+  EOF
+    fi
+
+    if [[ -n $renamed ]]; then
+      cat >> $msg <<EOF
+  Renamed:
+    $renamed
+  EOF
+    fi
+
+    if [[ -n $copied ]]; then
+      cat >> $msg <<EOF
+  Copied:
+    $copied
+  EOF
+    fi
+
+    if [[ -n $deleted ]]; then
+      cat >> $msg <<EOF
+  Deleted:
+    $deleted
+  EOF
+    fi
+
+    if [[ -n $ftchange ]]; then
+      cat >> $msg <<EOF
+  Filetype changed:
+    $ftchange
+  EOF
+    fi
+  }
+
   confirm() {
     while true; do
       read -sr -n 1 -p 'Commit with this message? [y\n]' REPLY
@@ -10,27 +104,20 @@
     done
   }
 
-  REPONAME=$(basename $PWD | tr -d '.')
-  TMPFILE=$(mktemp /tmp/commit-msg-$REPONAME.XXXXXXX)
+  main() {
+    git_status=$(git status --porcelain)
+    tmp=$(mktemp -p /tmp gitstat-json.XXXXXXXXXX)
+    msg=$(mktemp -p /tmp git-commit-msg.XXXXXXXXX)
 
-  git status --porcelain | grep '^[MARCDT]' | sort \
-  | sed -re 's/^([[:upper:]])[[:upper:]]?[[:space:]]+/\1:\n/' \
-  | awk '!x[$0]++' \
-  | sed -re 's/^([[:upper:]]:)$/\n\1/' \
-  | sed -re 's/^M:$/Modified: /' \
-  | sed -re 's/^A:$/Added: /' \
-  | sed -re 's/^R:$/Renamed: /' \
-  | sed -re 's/^C:$/Copied: /' \
-  | sed -re 's/^D:$/Deleted: /' \
-  | sed -re 's/^T:$/File Type Changed: /' \
-  | tr '\n' ' ' | xargs \
-  > $TMPFILE
+    (git_status_json | jq) > $tmp
+    parse_json 2>/dev/null
 
-  cat $TMPFILE
+    cat $msg
 
-  if confirm; then
-    git commit -F $TMPFILE
-  fi
+    if confirm; then
+      git commit -F $msg
+    fi
+  }
 
-  rm -f $TMPFILE
+  main || exit 1
 ''
