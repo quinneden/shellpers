1bcf71e7455478962f8d7c942ebe954b66881c21
Quinn Edenfield <quinnyxboy@gmail.com>
2024-10-29 04:47:06 -0700

    Modified: nix/overlay.nix

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -1,17 +1,17 @@
 final: prev: {
-  cfg = final.callPackage nix/cfg { inherit final; };
-  commit = final.callPackage nix/commit { inherit final; };
-  cop = final.callPackage nix/cop { inherit final; };
-  darwin-switch = final.callPackage nix/darwin-switch { inherit final; };
-  diskusage = final.callPackage nix/diskusage { inherit final; };
-  fuck = final.callPackage nix/fuck { inherit final; };
-  lsh = final.callPackage nix/lsh { inherit final; };
-  mi = final.callPackage nix/mi { inherit final; };
-  nish = final.callPackage nix/nish { inherit final; };
-  nix-clean = final.callPackage nix/nix-clean { inherit final; };
-  nix-switch = final.callPackage nix/nix-switch { inherit final; };
-  nix-get-sha256 = final.callPackage nix/nix-get-sha256 { inherit final; };
-  rm-result = final.callPackage nix/rm-result { inherit final; };
-  sec = final.callPackage nix/sec { inherit final; };
-  wipe-linux = final.callPackage nix/wipe-linux { inherit final; };
+  cfg = final.callPackage ./cfg { inherit final; };
+  commit = final.callPackage ./commit { inherit final; };
+  cop = final.callPackage ./cop { inherit final; };
+  darwin-switch = final.callPackage ./darwin-switch { inherit final; };
+  diskusage = final.callPackage ./diskusage { inherit final; };
+  fuck = final.callPackage ./fuck { inherit final; };
+  lsh = final.callPackage ./lsh { inherit final; };
+  mi = final.callPackage ./mi { inherit final; };
+  nish = final.callPackage ./nish { inherit final; };
+  nix-clean = final.callPackage ./nix-clean { inherit final; };
+  nix-switch = final.callPackage ./nix-switch { inherit final; };
+  nix-get-sha256 = final.callPackage ./nix-get-sha256 { inherit final; };
+  rm-result = final.callPackage ./rm-result { inherit final; };
+  sec = final.callPackage ./sec { inherit final; };
+  wipe-linux = final.callPackage ./wipe-linux { inherit final; };
 }
