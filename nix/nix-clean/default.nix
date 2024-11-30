1d17a437786154a12f25bff77bff945b98987c0e
quinneden <quinnyxboy@gmail.com>
2024-11-29 22:31:53 -0800

    Modified: nix/nix-clean/default.nix

--- nix/nix-clean/default.nix
+++ nix/nix-clean/default.nix
@@ -1,19 +1,18 @@
 {
+  lib,
   pkgs,
   stdenv,
   ...
 }:
 let
-  nix-clean = pkgs.writeShellScriptBin "nix-clean" ''
-    message_loop() {
-      while [[ $BREAK == 0 ]]; do
-        printf "\rCollecting garbage from nix store.  "; sleep 0.7
-        printf "\rCollecting garbage from nix store.. "; sleep 0.8
-        printf "\rCollecting garbage from nix store..."; sleep 0.9
-        printf "\rCollecting garbage from nix store   "; sleep 0.6
-      done
-    }
+  collect-garbage = pkgs.writeShellScriptBin "collect-garbage" ''
+    read -ra arr1 < <(nix-collect-garbage -d 2>/dev/null)
+    read -ra arr2 < <(sudo nix-collect-garbage -d 2>/dev/null)
+    read -r store_paths < <(awk "BEGIN {print ''${arr1[0]}+''${arr2[0]}; exit}")
+    read -r mib_float < <(awk "BEGIN {print ''${arr1[4]}+''${arr2[4]}; exit}")
+  '';
 
+  nix-clean = pkgs.writeShellScriptBin "nix-clean" ''
     collect_garbage() {
       read -ra arr1 < <(nix-collect-garbage -d 2>/dev/null)
       read -ra arr2 < <(sudo nix-collect-garbage -d 2>/dev/null)
@@ -21,18 +20,18 @@ let
       read -r mib_float < <(awk "BEGIN {print ''${arr1[4]}+''${arr2[4]}; exit}")
     }
 
-    main() {
-      local BREAK=0
-      message_loop
-      if collect_garbage; then
-        local BREAK=1; sleep 0.3
-        printf "\n$store_paths store paths deleted, $mib_float MiB saved.\n"
-      else
-        printf "error: garbage collection failed\n"; exit 1
-      fi
-    }
+    ${pkgs.gum}/bin/gum spin --show-error \
+      --title.foreground=220 \
+      --title="Collecting garbage..." \
+      --spinner=line \
+      --spinner.foreground=202 \
+      ${collect-garbage}/bin/collect-garbage
 
-    main || exit 1
+    if [[ $# -eq 0 ]]; then
+      printf "\n$store_paths store paths deleted, $mib_float MiB saved.\n"
+    else
+      printf "error: garbage collection failed\n"; exit 1
+    fi
   '';
 in
 stdenv.mkDerivation rec {
