290d64b4a96c53e676cd7177dca7533030b9104b
quinneden <quinnyxboy@gmail.com>
2024-12-08 23:03:40 -0800

    Modified: nix/nix-clean/default.nix

--- nix/nix-clean/default.nix
+++ nix/nix-clean/default.nix
@@ -2,36 +2,38 @@
   lib,
   pkgs,
   stdenv,
+  writeShellScript,
+  writeShellScriptBin,
   ...
 }:
 let
-  collect-garbage = pkgs.writeShellScriptBin "collect-garbage" ''
-    read -ra arr1 < <(nix-collect-garbage -d 2>/dev/null)
-    read -ra arr2 < <(sudo nix-collect-garbage -d 2>/dev/null)
+  collect-garbage = writeShellScript "collect-garbage" ''
+    read -ra arr1 < <(sudo nix-collect-garbage -d)
+    read -ra arr2 < <(nix-collect-garbage -d)
     read -r store_paths < <(awk "BEGIN {print ''${arr1[0]}+''${arr2[0]}; exit}")
     read -r mib_float < <(awk "BEGIN {print ''${arr1[4]}+''${arr2[4]}; exit}")
-    export arr1 arr2 store_paths mib_float
+    printf "$store_paths $mib_float"
   '';
 
-  nix-clean = pkgs.writeShellScriptBin "nix-clean" ''
-    collect_garbage() {
-      read -ra arr1 < <(nix-collect-garbage -d 2>/dev/null)
-      read -ra arr2 < <(sudo nix-collect-garbage -d 2>/dev/null)
-      read -r store_paths < <(awk "BEGIN {print ''${arr1[0]}+''${arr2[0]}; exit}")
-      read -r mib_float < <(awk "BEGIN {print ''${arr1[4]}+''${arr2[4]}; exit}")
-    }
+  nix-clean = writeShellScriptBin "nix-clean" ''
+    tmpfile="$(mktemp -p /tmp -t nix-clean)"
 
-    ${pkgs.gum}/bin/gum spin --show-error \
+    /nix/store/2kh20kzsh8p9b045janwracxwh43zzcf-gum-0.14.5/bin/gum spin --show-output \
       --title.foreground=220 \
       --title="Collecting garbage..." \
       --spinner=line \
       --spinner.foreground=202 \
-      ${collect-garbage}/bin/collect-garbage
+        ${collect-garbage} > "$tmpfile"
+
+    store_paths="$(cut -f1 -d' ' < $tmpfile)"
+    mib_float="$(cut -f2 -d' ' < $tmpfile)"
+
+    rm "$tmpfile"
 
     if [[ $# -eq 0 ]]; then
       printf "\n$store_paths store paths deleted, $mib_float MiB saved.\n"
     else
-      printf "error: garbage collection failed\n"; exit 1
+      printf "\nerror: garbage collection failed\n"; exit 1
     fi
   '';
 in
@@ -41,6 +43,6 @@ stdenv.mkDerivation rec {
   buildInputs = [ nix-clean ];
   installPhase = ''
     mkdir -p $out/bin
-    cp ${nix-clean}/bin/* $out/bin
+    cp ${lib.getExe nix-clean} $out/bin
   '';
 }
