3499565493167dec77b524c190a9018de0260c6a
quinneden <quinn@qeden.dev>
2025-04-24 14:53:46 -0700

    Modified: scripts/common/a2dl/package.nix

--- scripts/common/a2dl/package.nix
+++ scripts/common/a2dl/package.nix
@@ -9,13 +9,30 @@ let
 
   script = writeShellScript "a2dl" ''
     PATH="${binPath}:$PATH"
-
     URL="$1"
+
+    while [[ $# -gt 0 ]]; do
+      case "$1" in
+        -x)
+          max_conn="$1"
+          shift
+          ;;
+        -j)
+          max_dl="$1"
+          shift
+          ;;
+      esac
+    done
+
     if [[ "$URL" =~ ^http[s]*:// ]]; then
       output_dir="''${2:-$PWD}"
-      aria2c "$URL" -d "$output_dir"
+      aria2c "$URL" \
+        --max-connections-per-server=''${max_conn:-16} \
+        --max-concurrent-downloads=''${max_dl:-5} \
+        --dir="$output_dir"
     else
-      echo "error: malformed url"; exit 1
+      echo "error: malformed url" >&2
+      exit 1
     fi
   '';
 in
